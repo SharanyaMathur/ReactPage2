f8983c67dd28e289159f53a9ab8df006a6ee1ae3
SharanyaMathur <64073557+SharanyaMathur@users.noreply.github.com>
2021-09-10 22:41:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import NavBar from "./Components/NavBar";
+import './App.css';
+import Selector from "./Components/Selector";
+import Body from "./Components/Body";
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Body/>
+      <Selector/>
+    </div>
+  );
+}
+
+export default App;
